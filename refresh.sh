1810cc5457d38c77407f92c8d9d709cb42e7b33a
Nick Cooper <nickc@openai.com>
2024-03-21 01:20:28 -0400

    Initial commit. Just Pydantic V1 models

---
+++ refresh.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+GEN_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'k8')
+
+echo "Generating full client from OpenAPI spec..."
+npx @openapitools/openapi-generator-cli generate \
+    -i https://raw.githubusercontent.com/kubernetes-client/python/release-29.0/scripts/swagger.json \
+    -o $GEN_DIR \
+    -g python-pydantic-v1 \
+    -c $SCRIPT_DIR/config.json \
+    --skip-validate-spec
+
+
+TARGET_DIR=$SCRIPT_DIR/src
+
+echo "Copying client from $GEN_DIR to $TARGET_DIR..."
+rm -rf $TARGET_DIR
+mkdir -p $TARGET_DIR/k8
+mv $GEN_DIR/client/models/*.py $TARGET_DIR/k8
+
+# The default __init__.py includes all modules, this is an easy footgun as it's a large import cost
+# eliminate the option so clients must include individual models.
+echo "Removing __init__.py footgun..."
+echo "" > $TARGET_DIR/k8/__init__.py
+
+# Correct all import references
+echo "Correcting all import references..."
+find $TARGET_DIR -type f -name "*.py" -exec sed -i '' 's/client.models\./\./g' {} +
+
+# Ensure that Field default optional is actually optional
+echo "Correcting default arguments..."
+find $TARGET_DIR -type f -name "*.py" -exec sed -i '' 's/Field(None/Field(default=None/g' {} +
+
+echo "done."
